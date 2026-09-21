5cfdd832f01d3f704a0ad49b6cc8c57366fa0c77
Koxt <corentin.lavarde@gmail.com>
2026-09-21 17:10:53 +0200

    dbcontext configuré

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using TodoApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoDbContext> (options =>
+{
+   options.UseSqlite(builder.Configuration.GetConnectionString("Todo") ?? "Data Source=todos.db"); 
+});
 
 var app = builder.Build();
 
--- obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285912cc88c12f0b853befed9a893522b35488b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa818d4a332c50354df7caf8e31b5d32dc0c4334")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
