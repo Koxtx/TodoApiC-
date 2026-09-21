285912cc88c12f0b853befed9a893522b35488b0
Koxt <corentin.lavarde@gmail.com>
2026-09-21 16:07:53 +0200

    Controller test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers;
+
+[ApiController]
+[Route("api/test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetTest()
+    {
+        return Ok("Succesfull test !");
+    }
+}
